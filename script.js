0bcdc0a31c719cf3520eae7047ef0f5490ee7391
Jules Taptso <jules.taptso@psgdover.com>
2022-03-24 03:58:46 +0100

    get date value from submit

--- script.js
+++ script.js
@@ -2,6 +2,18 @@ const inputContainer = document.getElementById('input-container');
 const countdownForm = document.getElementById('countdownForm');
 const dateEl = document.getElementById('date-picker');
 
+let countdownTitle = '';
+let countdownDate = '';
 // Set Date Input Min with Taoday's Date
 const today = new Date().toISOString().split('T')[0];
 dateEl.setAttribute('min', today);
+
+// Take Values from Form Input
+function updateCountdown(e) {
+    e.preventDefault();
+    countdownTitle = e.srcElement[0].value;
+    countdownDate = e.srcElement[1].value;
+}
+
+// Event Listeners
+countdownForm.addEventListener('submit', updateCountdown)
